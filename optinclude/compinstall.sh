8dcb003ad6f9f5ef02c2ae6e7224e182883084b1
Olawale Adekoya <olawaley2k@gmail.com>
2025-01-30 14:27:56 -0500

    update

--- optinclude/compinstall.sh
+++ optinclude/compinstall.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 composer install --working-dir=..
+
+drush site:install -y \
+    --db-url=mysql://drupal:drupal@db/drupal \
+    --site-name="Drupal QA Demo" \
+    --account-name=drupal \
+    --account-mail=drupal@example.org \
+    --account-pass=drupal
